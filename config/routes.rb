38dd27ab703fe2e4c00559a6ff9b959ef15b741d
wangxing <18912540921@163.com>
2017-03-27 21:01:49 +0800

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root 'topics#index'
+  resources :topics do
+    member do
+      post 'upvote'
+      post 'downvote'
+    end
+  end
+  get 'about' => 'topics#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
